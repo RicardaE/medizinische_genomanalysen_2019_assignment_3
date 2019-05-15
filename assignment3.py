9e043c2ee71d0750f37fd42c0605ef28b9359bf7
Ricarda Erhart <ricarda.erhart@gmail.com>
2019-05-15 10:01:34 +0200

    initialized

--- assignment3.py
+++ assignment3.py
@@ -3,7 +3,7 @@
 import vcf
 import httplib2
 
-__author__ = 'XXX'
+__author__ = 'Ricarda_Erhart'
 
 
 ##
@@ -19,13 +19,13 @@ __author__ = 'XXX'
 ##
 
 class Assignment3:
-    
+
     def __init__(self):
         ## Check if pyvcf is installed
         print("PyVCF version: %s" % vcf.VERSION)
-        
+
         ## Call annotate_vcf_file here
-        self.vcf_path = ""  # TODO
+        self.vcf_path = "chr16.vcf"
 
     def annotate_vcf_file(self):
         '''
@@ -33,101 +33,96 @@ class Assignment3:
         - Iterate of the variants (use first 900)
         - Store the result in a data structure
         :return:
-        '''    
+        '''
         print("TODO")
-                
+
         ##
         ## Example loop
         ##
-        
+
         ## Build the connection
         h = httplib2.Http()
         headers = {'content-type': 'application/x-www-form-urlencoded'}
-                
+
         params_pos = []  # List of variant positions
         with open(self.vcf_path) as my_vcf_fh:
             vcf_reader = vcf.Reader(my_vcf_fh)
             for counter, record in enumerate(vcf_reader):
                 params_pos.append(record.CHROM + ":g." + str(record.POS) + record.REF + ">" + str(record.ALT[0]))
-                
+
                 if counter >= 899:
                     break
-        
+
         ## Build the parameters using the list we just built
         params = 'ids=' + ",".join(params_pos) + '&hg38=true'
-        
+
         ## Perform annotation
         res, con = h.request('http://myvariant.info/v1/variant', 'POST', params, headers=headers)
         annotation_result = con.decode('utf-8')
-        
+
         ## TODO now do something with the 'annotation_result'
-        
+
         ##
         ## End example code
         ##
-        
+
         return None  ## return the data structure here
-    
-    
+
+
     def get_list_of_genes(self):
         '''
         Print the name of genes in the annotation data set
         :return:
         '''
         print("TODO")
-    
-    
+
+
     def get_num_variants_modifier(self):
         '''
         Print the number of variants with putative_impact "MODIFIER"
         :return:
         '''
         print("TODO")
-        
-    
+
+
     def get_num_variants_with_mutationtaster_annotation(self):
         '''
         Print the number of variants with a 'mutationtaster' annotation
         :return:
         '''
         print("TODO")
-        
-    
+
+
     def get_num_variants_non_synonymous(self):
         '''
         Print the number of variants with 'consequence' 'NON_SYNONYMOUS'
         :return:
         '''
         print("TODO")
-        
-    
+
+
     def view_vcf_in_browser(self):
         '''
         - Open a browser and go to https://vcf.iobio.io/
         - Upload the VCF file and investigate the details
         :return:
         '''
-   
+
         ## Document the final URL here
         print("TODO")
-            
-    
+
+
     def print_summary(self):
         self.annotate_vcf_file()
         print("Print all results here")
-    
-    
+
+
 def main():
     print("Assignment 3")
     assignment3 = Assignment3()
     assignment3.print_summary()
     print("Done with assignment 3")
-        
-        
+
+
 if __name__ == '__main__':
     main()
-   
-    
-
-
-
